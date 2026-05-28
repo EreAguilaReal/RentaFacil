e3bd1ca84dbafbc3140a897657c7d931c39521a5
rodba123 <rodba123.rc@gmail.com>
2026-05-28 14:50:27 -0600

    Se añadio vista admin.py para la app usuarios

--- backend/usuarios/admin.py
+++ backend/usuarios/admin.py
@@ -1,3 +1,26 @@
+# usuarios/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Usuario
 
-# Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    ordering     = ('correo_electronico',)
+    list_display = ('correo_electronico', 'nombre_usuario', 'tipo_usuario', 'is_active', 'is_staff')
+
+    fieldsets = (
+        (None,            {'fields': ('correo_electronico', 'password')}),
+        ('Información',   {'fields': ('nombre_usuario', 'nombres', 'apellidos',
+                                      'fecha_nacimiento', 'genero', 'tipo_usuario',
+                                      'documento_verificacion')}),
+        ('Permisos',      {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Fechas',        {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields':  ('correo_electronico', 'nombre_usuario', 'nombres',
+                        'apellidos', 'password1', 'password2'),
+        }),
+    )
